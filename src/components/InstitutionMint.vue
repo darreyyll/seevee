a8f95911b569b4fb0366ae10dc5ccb10b48f1fd6
VibhuKrovvidi <vibkrovvidi@gmail.com>
2021-04-02 17:37:02 +0800

    Added scoring

--- src/components/InstitutionMint.vue
+++ src/components/InstitutionMint.vue
@@ -7,7 +7,19 @@
     <button class="switch" v-on:click="showExp"> Experience </button>
     <div v-if="this.switch.acad">
     <br>
-    <form>
+    <div class="holdinstdetails">
+    <h3 > Academic Claim </h3>
+    <label > Enter Claim ID </label><br>
+    <input v-model="claimId" @change="checkClaimId"><br>
+    <i>{{claimId}}</i> <br><br>
+    <label > Enter Candidate's Address </label><br>
+    <input v-model="candidateAddress"><br>
+    <i>{{candidateAddress}}</i>
+    </div>
+    <div v-if="valid_claimid">
+    <upload-claim v-on:submitted="submit_claim"> </upload-claim>
+    </div>
+    <!-- <form>
         <label> Claim ID: </label>
         <input type="number" v-model="acad.id" placeholder="ClaimID"/> <br>
         <label> Candidate: </label>
@@ -18,12 +30,20 @@
         <input type="text" v-model="acad.grade" placeholder="Attained Grade"/> <br>
         <br>
         <button v-on:click.prevent="verifyAcad"> Verify Academic Credential </button>
-    </form>
+    </form> -->
+
     <br>
     </div>
     <div v-if="this.switch.exp">
     <br>
-    <form>
+    <div class="holdinstdetails">
+    <h3 > Experience Claim </h3>
+    <label > Enter Claim ID </label><br>
+    <input v-model="claimId"><br>
+    <i>{{claimId}}</i>
+    </div>
+    <upload-claim v-on:submitted="submit_claim"> </upload-claim>
+    <!-- <form>
         <label> Claim ID: </label>
         <input type="number" v-model="exp.id" placeholder="ClaimID"/> <br>
         <label> Candidate: </label>
@@ -38,9 +58,10 @@
         <input type="text" v-model="exp.comments" placeholder="Comments"/> <br>
         <br>
         <button v-on:click.prevent="verifyExp"> Verify Experience Credential </button>
-    </form>
+    </form> -->
     <br>
     </div>
+    <b v-if="computingscore"> Computing Score. Please do not close this browser </b>
     <br><hr>
     <p> <b> Revoke Credential </b> </p>
     <form>
@@ -56,6 +77,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import database from '../firebase.js' //to log our data
+import UploadClaim from './UploadClaim2.vue';
 
 export default {
   name: "app",
@@ -102,6 +124,101 @@ export default {
       async revokeCred() {
           await this.drizzleInstance.contracts.Credential.methods.revoke(this.revoke.id).send();
       },
+      async checkClaimId() {
+          try {
+            var check = await this.drizzleInstance.contracts.Credential.methods.viewClaim(this.claimId).call();
+            // console.log("CHECK = ", check);
+            this.hsh = check;
+            this.valid_claimid = true;
+          } catch(err) {
+              console.log("err")
+              this.claimId = "INVALID"
+          }
+
+      },
+
+      submit_claim(json_file) {
+          
+          this.counterclaim = json_file;
+          console.log(this.counterclaim);
+          console.log(JSON.stringify(this.counterclaim))
+          this.computeScore()
+      },
+
+     async computeScore() {
+         
+         if(this.switch.acad) {
+            
+             await database.collection('students').doc(this.candidateAddress).collection("acads").doc(this.hsh).get().then((doc) => {
+                 if(doc.exists) {
+                    this.claim = doc.data()
+                    this.claim = this.claim["claim_contents"]
+                 }
+             }).catch((error) => {
+                    console.log("Error getting document:", error);
+                });
+
+
+         } else {
+            await database.collection('students').doc(this.candidateAddress).collection("exp").doc(this.hsh).get().then((doc) => {
+                 if(doc.exists) {
+                     console.log("DOC = ", doc.data())
+                    this.claim = doc.data()["claim_contents"];
+                 }
+             }).catch((error) => {
+                    console.log("Error getting document:", error);
+                });
+
+         }
+        console.log("CLAIM = ", this.claim)
+        // data = claim
+        //this.counterclaim = counterclaim
+
+    
+
+        for(var key in this.counterclaim) {
+            // console.log("KEY " , key)
+            // console.log("VALUE", this.counterclaim[key])
+            try {
+                let field = this.claim[key]
+                // console.log("FIELD", field)
+                let score = this.scoreAlgo(this.counterclaim[key], field, this.counterclaim[key].length, field.length);
+                console.log(key, " SCORE = ", score);
+                this.total_score += score;
+                this.total_potential += this.counterclaim[key].length;
+            } catch(e) {
+                console.log("No such field")
+                this.total_score -= 50; //punitive measure
+            }
+        }
+
+        console.log("Total Score", this.total_potential - this.total_score, " Total Potential", this.total_potential)
+        this.final_score = (this.total_potential - this.total_score)/this.total_potential;
+        this.final_score = this.final_score * 100;
+        console.log("Final Score = ", this.final_score)
+
+
+    },
+
+    scoreAlgo(field1, field2, m, n) {
+
+        if (m == 0)
+            return n;
+
+        if (n == 0)
+            return m;
+ 
+        if (field1.charAt(m - 1) == field2.charAt(n - 1))
+            return this.scoreAlgo(field1, field2, m - 1, n - 1);
+ 
+        return 1
+            + Math.min(this.scoreAlgo(field1, field2, m, n - 1), // Insert
+                  this.scoreAlgo(field1, field2, m - 1, n), // Remove
+                  this.scoreAlgo(field1, field2, m - 1,
+                           n - 1) // Replace
+              );
+    }
+    
   },
   data() {
       return {
@@ -110,6 +227,7 @@ export default {
               acad: true,
               exp: false,
           },
+          claimId: "",
           acad: {
               id: '',
               user: '',
@@ -130,8 +248,20 @@ export default {
               id: '',
               exp: false,
           },
+          counterclaim: {},
+          claim: {},
+          computingscore : false,
+          valid_claimid : false,
+          candidateAddress : "",
+          hsh : "",
+          total_score : 0,
+          total_potential: 0,
+          final_score: 0
       }
   },
+  components : {
+      UploadClaim
+  }
 }
 </script>
 
